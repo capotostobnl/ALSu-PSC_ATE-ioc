21cb7e831c67fe072391398c8e193610ca433775
Michael Capotosto <capotosto@bnl.gov>
2025-11-12 15:17:19 -0500

    Fixes to Tester2.c, BPC opi

--- st.cmd
+++ st.cmd
@@ -1,4 +1,8 @@
 #!/bin/bash
+<<<<<<< HEAD
 cd "/epics/iocs/PSCTest2/iocBoot/iocPSCtest"
+=======
+cd "/epics/iocs/PSCTest/iocBoot/iocPSCtest"
+>>>>>>> origin/dev
 
 ../../bin/linux-x86_64/PSCtest st.cmd
